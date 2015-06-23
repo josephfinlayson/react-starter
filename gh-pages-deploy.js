c0c55672466ad50e9a8b19ec4029bbdac9e7d956
Joseph Finlayson <joseph.finlayson@gmail.com>
2015-06-23 22:27:45 +0200

    using force to account for there already being an index file in build

--- gh-pages-deploy.js
+++ gh-pages-deploy.js
@@ -7,9 +7,9 @@ if (!which('git')) {
 
 exec('git stash');
 var commitDate = new Date().toString();
-cp('lib/staticSite.html', 'build/public/index.html');
+cp('-f', 'lib/staticSite.html', 'build/public/index.html');
 exec('git branch gh-pages');
 exec('git checkout gh-pages');
-cp('-r', 'build/public/*', '.');
+cp('-rf', 'build/public/*', '.');
 exec('git add .');
 exec('git commit -m "Automated commit on' + commitDate);
